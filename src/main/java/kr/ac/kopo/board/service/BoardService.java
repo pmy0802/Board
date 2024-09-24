3dcf1f81d5ab99a9a16131b67d5497480599a724
pmy0802 <pmy0802@naver.com>
2024-09-24 14:39:54 +0900

    BoardService에 getList() BoardServiceImpl get List() 구현

--- src/main/java/kr/ac/kopo/board/service/BoardService.java
+++ src/main/java/kr/ac/kopo/board/service/BoardService.java
@@ -1,12 +1,19 @@
 package kr.ac.kopo.board.service;
 
 import kr.ac.kopo.board.DTO.BoardDTO;
+import kr.ac.kopo.board.DTO.PageRequestDTO;
+import kr.ac.kopo.board.DTO.PageResultDTO;
 import kr.ac.kopo.board.entity.Board;
 import kr.ac.kopo.board.entity.Member;
+
+import java.util.Objects;
 
 public interface BoardService {
     // 새글을 등록하는 기능
     Long register(BoardDTO dto);
+
+    // 게시목록 처리 기능
+    PageResultDTO<BoardDTO, Object[]> getList(PageRequestDTO pageRequestDTO);
 
     // Entity를 DTO로 변환하는 메소드
     default BoardDTO entityToDTO(Board board, Member member, Long replyCount){
--- src/main/java/kr/ac/kopo/board/service/BoardServiceimpl.java
+++ src/main/java/kr/ac/kopo/board/service/BoardServiceimpl.java
@@ -1,14 +1,21 @@
 package kr.ac.kopo.board.service;
 
 import kr.ac.kopo.board.DTO.BoardDTO;
+import kr.ac.kopo.board.DTO.PageRequestDTO;
+import kr.ac.kopo.board.DTO.PageResultDTO;
 import kr.ac.kopo.board.entity.Board;
+import kr.ac.kopo.board.entity.Member;
 import kr.ac.kopo.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
 
 @Service
 @RequiredArgsConstructor
-public class BoardServiceimpl implements BoardService{
+public class BoardServiceImpl implements BoardService{
     private final BoardRepository repository;
     @Override
     public Long register(BoardDTO dto) {
@@ -17,4 +24,14 @@ public class BoardServiceimpl implements BoardService{
 
         return board.getBno();
     }
+
+    @Override
+    public PageResultDTO<BoardDTO, Object[]> getList(PageRequestDTO pageRequestDTO) {
+        Function<Object[], BoardDTO> fn = (en -> entityToDTO((Board) en[0], (Member) en[1],(Long) en[2]));
+        Page<Object[]> result = repository.getBoardWithReplyCount(pageRequestDTO.getPageable(Sort.by("bno").descending()));
+
+        return new PageResultDTO<>(result, fn);
+    }
+
+
 }
