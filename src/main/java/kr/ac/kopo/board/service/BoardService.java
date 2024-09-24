cfae63f77235329892d885f74c8c1db002abbdc2
pmy0802 <pmy0802@naver.com>
2024-09-24 14:10:58 +0900

    BoardService에 entityToDTO추가

--- src/main/java/kr/ac/kopo/board/service/BoardService.java
+++ src/main/java/kr/ac/kopo/board/service/BoardService.java
@@ -8,6 +8,26 @@ public interface BoardService {
     // 새글을 등록하는 기능
     Long register(BoardDTO dto);
 
+    // Entity를 DTO로 변환하는 메소드
+    default BoardDTO entityToDTO(Board board, Member member, Long replyCount){
+        BoardDTO boardDTO = BoardDTO.builder()
+                .bno(board.getBno())
+                .title(board.getTitle())
+                .content(board.getContent())
+                .regDate(board.getRegDate())
+                .modDate(board.getModDate())
+                .writerEmail(member.getEmail())
+                .writerName(member.getName())
+                .replyCount(replyCount.intValue())
+                .build();
+
+        return boardDTO;
+    }
+
+    // DTO를 Entity로 변환하는 메소드
+
+
+
     default Board dtoToEntity(BoardDTO dto){
         Member member = Member.builder()
                 .email(dto.getWriterEmail())
